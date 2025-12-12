94f30778315286443a6a937ad4a0f0a05bbe909e
Troy-Davies <164232406+troy-davies@users.noreply.github.com>
2025-12-12 22:06:28 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,58 @@
+let Clock = 0 // Clock by Troy Davies
+let Minute = 0
+let Hour = 0
+let SetupComplete = 0
+let Locale = 2 // To use 12 hour, use Locale = 1
+function SetTime () {
+    while (SetupComplete != 1) {
+        if (input.buttonIsPressed(Button.A)) {
+            Hour += 1
+            if (12 * Locale < Hour) {
+                Hour = 0
+            }
+            ShowTime()
+        }
+        if (input.buttonIsPressed(Button.B)) {
+            Minute += 5
+            if (60 < Minute) {
+                Minute = 0
+            }
+            ShowTime()
+        }
+        if (input.buttonIsPressed(Button.AB)) {
+            SetupComplete = 1
+        }
+    }
+}
+function Tick () {
+    basic.pause(1)
+    Clock += 1
+    if (Clock == 60000) {
+        Clock = 0
+        Minute += 1
+    }
+    if (Minute == 60) {
+        Minute = 0
+        Hour += 1
+    }
+    if (Hour == 12 * Locale) {
+        Hour = 0
+    }
+}
+function ShowTime () {
+    if (Minute < 10) {
+        basic.showString("" + Hour + ":" + "0" + Minute)
+    } else {
+        basic.showString("" + Hour + ":" + Minute)
+    }
+}
+basic.showString("Calibrate")
+SetTime()
+basic.forever(function () {
+    if (SetupComplete == 1) {
+        if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
+            ShowTime()
+        }
+        Tick()
+    }
+})

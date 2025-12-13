0aa11b470d2cf7145442d0d2d811ce194e8daa61
Troy-Davies <164232406+troy-davies@users.noreply.github.com>
2025-12-13 08:52:10 +0000

    On the way to fixing bugs

--- main.ts
+++ main.ts
@@ -2,7 +2,9 @@ let Clock = 0 // Clock by Troy Davies
 let Minute = 0
 let Hour = 0
 let SetupComplete = 0
-let Locale = 2 // To use 12 hour, use Locale = 1
+let Locale = 0 // To use 12 hour, use Locale = 1
+basic.showString("Calibrate")
+SetTime()
 function SetTime () {
     while (SetupComplete != 1) {
         if (input.buttonIsPressed(Button.A)) {
@@ -31,28 +33,26 @@ function Tick () {
         Clock = 0
         Minute += 1
     }
-    if (Minute == 60) {
+    if (Minute > 60) {
         Minute = 0
         Hour += 1
     }
-    if (Hour == 12 * Locale) {
+    if (Hour > 12 * Locale) {
         Hour = 0
     }
 }
 function ShowTime () {
     if (Minute < 10) {
-        basic.showString("" + Hour + ":" + "0" + Minute)
+        basic.showString(Hour + ":" + "0" + Minute)
     } else {
-        basic.showString("" + Hour + ":" + Minute)
+        basic.showString(Hour + ":" + Minute)
     }
 }
-basic.showString("Calibrate")
-SetTime()
 basic.forever(function () {
+    Tick()
     if (SetupComplete == 1) {
         if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
             ShowTime()
         }
-        Tick()
     }
 })
